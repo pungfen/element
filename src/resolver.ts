12cc276bbbc29c15e3249dccf6011cf1d753a469
peng <fp94214@gmail.com>
2026-02-06 16:16:35 +0800

    xx

--- src/resolver.ts
+++ src/resolver.ts
@@ -25,22 +25,24 @@ const names = [
   ['XUpload', 'ElUpload']
 ]
 
-export const kebabCase = (str: string) => str.replace(/([A-Z])/g, ' $1').trim().split(' ').join('-').toLowerCase()
+const kebabCase = (str: string) => str.replace(/([A-Z])/g, ' $1').trim().split(' ').join('-').toLowerCase()
 
 export default () => {
-  return {
-    type: 'component',
-    resolve: (name) => {
-      if (names.flatMap(it => it[0]).includes(name)) {
-        return {
-          name,
-          from: '@pungfe/element',
-          sideEffects: [
-            'element-plus/es/components/base/style/css',
-            `element-plus/es/components/${kebabCase(names.find(it => it[0] === name)![1]!.slice(2))}/style/css`
-          ]
+  return [
+    {
+      type: 'component',
+      resolve: (name) => {
+        if (names.flatMap(it => it[0]).includes(name)) {
+          return {
+            name,
+            from: '@pungfe/element',
+            sideEffects: [
+              'element-plus/es/components/base/style/css',
+              `element-plus/es/components/${kebabCase(names.find(it => it[0] === name)![1]!.slice(2))}/style/css`
+            ]
+          }
         }
       }
     }
-  } as ComponentResolver
+  ] as ComponentResolver[]
 }
